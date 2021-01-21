7352b47f0c4b0d5d821f3865fe6cc490135d8063
AliOzgurDede <74831928+AliOzgurDede@users.noreply.github.com>
2021-01-21 23:19:12 +0300

    Update DescriptiveStatistics.cs

--- Code/DescriptiveStatistics.cs
+++ Code/DescriptiveStatistics.cs
@@ -1,7 +1,7 @@
 /* 
 The MIT License(MIT)
 
-Copyright(c) 2020 Ali Özgür Dede
+Copyright(c) 2020 Ali Ozgur Dede
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
